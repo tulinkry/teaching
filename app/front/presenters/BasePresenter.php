b7f6e38769d4b2d92aa3e618a62e72d880e402da
Kryštof Tulinger <tulinkry@fit.cvut.cz>
2016-09-25 12:40:01 +0200

    adding last updated info

--- app/front/presenters/BasePresenter.php
+++ app/front/presenters/BasePresenter.php
@@ -3,6 +3,8 @@
 namespace App\FrontModule\Presenters;
 
 use Tulinkry;
+use Nette\Utils\DateTime;
+use Nette\Utils\FileSystem;
 use App;
 
 class BasePresenter extends Tulinkry\Application\UI\Presenter
@@ -10,6 +12,18 @@ class BasePresenter extends Tulinkry\Application\UI\Presenter
 	public function startup () {
 		parent::startup();
 		$this->template->top_menu = $this->parameters->menu;
+		$timestamp = $this->parameters->params['appDir'] . DIRECTORY_SEPARATOR . 'timestamp';
+		if(file_exists($timestamp)) {
+			$this->template->lastUpdated = DateTime::from(@filemtime($timestamp));
+		}
+	}
+
+	public static function update() {
+		$timestamp = APP_DIR . DIRECTORY_SEPARATOR . 'timestamp';
+		if(file_exists($timestamp)) {
+			FileSystem::delete($timestamp);
+		}
+		FileSystem::write($timestamp);
 	}
 
 	protected function createComponentCodeControl ($name) {
